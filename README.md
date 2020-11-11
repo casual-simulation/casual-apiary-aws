7d7021160906572ca6fa90298bbc36e70f738cb3
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-11-11 14:38:55 -0500

    chore: Format all the files

--- README.md
+++ README.md
@@ -5,18 +5,25 @@ A Serverless AWS project that can host many CasualOS instances at once.
 ## Setup
 
 1. Install the [Serverless framework](https://www.serverless.com/framework/docs/getting-started/)
+
 ```bash
 $ npm install -g serverless
 ```
+
 2. Install dependencies with Yarn.
+
 ```bash
 $ yarn
 ```
+
 3. Make sure you have the [AWS CLI](https://aws.amazon.com/cli/) and [the `casualsimulation` profile is configured](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html#cli-configure-quickstart-profiles):
+
 ```bash
 $ aws configure --profile casualsimulation
 ```
+
 4. Deploy to AWS
+
 ```bash
 $ serverless deploy
 ```
--- serverless.ts
+++ serverless.ts
@@ -1,43 +1,43 @@
 import type { Serverless, ApiGateway } from 'serverless/aws';
 
 const serverlessConfiguration: Serverless = {
-  service: 'casual-apiary-aws',
-  org: 'kallyngowdy',
-  app: 'casual-apiary',
-  frameworkVersion: '2',
-  custom: {
-    webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
-  },
-  // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
-    profile: 'casualsimulation',
-    apiGateway: {
-      shouldStartNameWithService: true,
-      minimumCompressionSize: 1024,
-    } as ApiGateway,
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    service: 'casual-apiary-aws',
+    org: 'kallyngowdy',
+    app: 'casual-apiary',
+    frameworkVersion: '2',
+    custom: {
+        webpack: {
+            webpackConfig: './webpack.config.js',
+            includeModules: true,
+        },
     },
-  },
-  functions: {
-    hello: {
-      handler: 'handler.hello',
-      events: [
-        {
-          http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+    // Add the serverless-webpack plugin
+    plugins: ['serverless-webpack'],
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs12.x',
+        profile: 'casualsimulation',
+        apiGateway: {
+            shouldStartNameWithService: true,
+            minimumCompressionSize: 1024,
+        } as ApiGateway,
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+        },
+    },
+    functions: {
+        hello: {
+            handler: 'handler.hello',
+            events: [
+                {
+                    http: {
+                        method: 'get',
+                        path: 'hello',
+                    },
+                },
+            ],
+        },
+    },
+};
 
 module.exports = serverlessConfiguration;
